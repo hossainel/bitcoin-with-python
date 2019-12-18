0713f0fd0f780bab8c41f0423f715971d136ec5b
Md Safayet El Hossain <31356820+hossainel@users.noreply.github.com>
2019-12-18 15:37:32 +0600

    Update and rename btc_mutiSig..py to btc_muti_sig.py

--- btc_muti_sig.py
+++ btc_muti_sig.py
@@ -11,8 +11,10 @@ multi_sig = bt.mk_multisig_script(private_key1, private_key2, private_key3, 2,3)
 btc_address = scriptaddr(multi_sig)
 
 #with open('data.txt', 'w') as fob:
-#    fob.write('Private Key: 'private_key+'\nBTC Address: '+bitcoin_address+'\n')
+#    fob.write('Private Key1: ' + private_key1 + '\nPrivate Key2: ' + private_key2 + '\nPrivate Key3: ' + private_key3 + '\nBTC  Address: '+bitcoin_address+'\n')
 #    fob.close()
 
-print('Private Key:', private_key)
-print('BTC Address:', btc_address)
+print('Private Key1:', private_key1)
+print('Private Key2:', private_key2)
+print('Private Key3:', private_key3)
+print('BTC  Address:', btc_address)
