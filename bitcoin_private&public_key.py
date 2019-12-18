8610561ba59ef65cbb2d7bb310ec4127bd57ce0c
Md Safayet El Hossain <31356820+hossainel@users.noreply.github.com>
2019-12-18 14:59:16 +0600

    Update bitcoin_private&public_key.py

--- bitcoin_private&public_key.py
+++ bitcoin_private&public_key.py
@@ -1,10 +1,13 @@
 import bitcoin as bt
 #private Key
 private_key = bt.random_key()
+#public Key
 public_key = bt.privtopub(private_key)
-with open('data.txt', 'w') as fob:
-    fob.write(private_key+'\n'+public_key)
-    fob.close()
+
+#also generates a text file if you like to save that
+#with open('data.txt', 'w') as fob:
+#    fob.write('Private Key: '+private_key+'\nPublic  Key: '+public_key+'\n')
+#    fob.close()
 
 print('Private Key:', private_key)
 print('Public Key:', public_key)
