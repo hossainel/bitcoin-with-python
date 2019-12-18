3768978a890175789bed8d911498750f313173d7
Md Safayet El Hossain <31356820+hossainel@users.noreply.github.com>
2019-12-18 15:16:34 +0600

    Update and rename bitcoin_private&public_key.py to btc_key&address.py

--- btc_key&address.py
+++ btc_key&address.py
@@ -3,11 +3,13 @@ import bitcoin as bt
 private_key = bt.random_key()
 #public Key
 public_key = bt.privtopub(private_key)
-
+#bitcoin Address
+btc_address = bt.pubtoaddr(public_key)
 #also generates a text file if you like to save that
 #with open('data.txt', 'w') as fob:
-#    fob.write('Private Key: '+private_key+'\nPublic  Key: '+public_key+'\n')
+#    fob.write('Private Key: '+private_key+'\nPublic  Key: '+public_key+'\nBitcoin Address: '+btc_address+'\n')
 #    fob.close()
 
 print('Private Key:', private_key)
 print('Public Key:', public_key)
+print('Bitcoin Address:', btc_address)
