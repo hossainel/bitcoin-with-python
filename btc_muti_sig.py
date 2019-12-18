a8100ccbffcd89cc6b5bb7c982332801eddb6f59
Md Safayet El Hossain <31356820+hossainel@users.noreply.github.com>
2019-12-18 15:58:32 +0600

    Update btc_muti_sig.py

--- btc_muti_sig.py
+++ btc_muti_sig.py
@@ -2,16 +2,19 @@ import bitcoin as bt
 
 #1st Sig
 private_key1 = bt.random_key()
+public_key1 = bt.privttopub(private_key1)
 #2nd Sig
 private_key2 = bt.random_key()
+public_key2 = bt.privttopub(private_key2)
 #3rd Sig
 private_key3 = bt.random_key()
+public_key3 = bt.privttopub(private_key3)
 
-multi_sig = bt.mk_multisig_script(private_key1, private_key2, private_key3, 2,3)
+multi_sig = bt.mk_multisig_script(public_key1, public_key2, public_key3, 2,3)
 btc_address = scriptaddr(multi_sig)
 
 #with open('data.txt', 'w') as fob:
-#    fob.write('Private Key1: ' + private_key1 + '\nPrivate Key2: ' + private_key2 + '\nPrivate Key3: ' + private_key3 + '\nBTC  Address: '+bitcoin_address+'\n')
+#    fob.write('Private Key1: ' + private_key1 + '\nPrivate Key2: ' + private_key2 + '\nPrivate Key3: ' + private_key3 + 'nPublic Key1: ' + public_key1 + '\nPublic Key2: ' + public_key2 + '\nPublic Key3: ' + public_key3 + '\nBTC  Address: '+bitcoin_address+'\n')
 #    fob.close()
 
 print('Private Key1:', private_key1)
